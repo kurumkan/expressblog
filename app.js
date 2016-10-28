a9b84c35f57357fb64eebc392e92e6a1cfe5f5e8
kurumkan <joomrise@gmail.com>
2016-10-28 18:14:58 +0800

    added body-parser module

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
+
+
+mongoose.connect('mongodb://localhost/restful_blog_app')
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("Server started");
+})
